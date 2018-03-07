b64d5918206ec0dde605c3a576cbbf847e0ba143
Cody Windeknecht <lhg614@mocs.utc.edu>
2018-03-07 16:23:36 -0500

    Requirements Complete

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const fetch = require('node-fetch');
 
 const config = require('./config.js');
 
 const API_KEY = config.gmaps.apiKey;
 
+const STATUS_USER_ERROR = 422;
+const STATUS_OKAY = 200;
+const PORT = 3000;
+
 const server = express();
+server.use(bodyParser.json());
 
-fetch(`https://maps.googleapis.com/maps/api/place/textsearch/xml?query=coffee+shops+in+Austin=${API_KEY}`).
-    then(res => res.json()).
-    then(json => console.log(json));
+server
+  .get('/place', (req, res) => {
+    const { term } = req.query;
+    const searchTerm = term.split(' ').join('+');
+    fetch(
+      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${searchTerm}&key=${API_KEY}`
+    )
+      .then(res => res.json())
+      .then(places => {
+        const place = places.results[0];
+        // return fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${id}&key=${API_KEY}`).
+        // then(res => res.json()).
+        // then(json => console.log(json)).
+        // then(details => {
+        //     console.log('Details',details.result);
+        //     res.send(details.result);
+        //     return;
+        res.json(place);
+      })
+      .catch(err => console.error(err));
+  })
 
-fetch('https://maps.googleapis.com/maps/api/place/textsearch/xml?query=coffee+shops+in+Austin=${API_KEY}').
-    catch(err => console.error(err));
+  server.listen(PORT);
