67325082495285cec62492e8ffc6b7faf930ad28
das41236 <davsoudry@gmail.com>
2018-03-07 15:59:53 -0600

    requirements complete updated

--- server.js
+++ server.js
@@ -9,30 +9,39 @@ const API_KEY = config.gmaps.apiKey;
 const STATUS_USER_ERROR = 422;
 const STATUS_OKAY = 200;
 const PORT = 3000;
+const URL = 'https://maps.googleapis.com/maps/api/place';
 
 const server = express();
 server.use(bodyParser.json());
 
-server
-  .get('/place', (req, res) => {
+server.
+    get('/place', (req, res) => {
     const { term } = req.query;
     const searchTerm = term.split(' ').join('+');
     fetch(
-      `https://maps.googleapis.com/maps/api/place/textsearch/json?query=${searchTerm}&key=${API_KEY}`
-    )
-      .then(res => res.json())
-      .then(places => {
-        const place = places.results[0];
-        // return fetch(`https://maps.googleapis.com/maps/api/place/details/json?placeid=${id}&key=${API_KEY}`).
-        // then(res => res.json()).
-        // then(json => console.log(json)).
-        // then(details => {
-        //     console.log('Details',details.result);
-        //     res.send(details.result);
-        //     return;
-        res.json(place);
+      `${URL}/textsearch/json?query=${searchTerm}&key=${API_KEY}`).
+      then(res => res.json()).
+      then(detail => detail.results[0].place_id).
+      then(place => {
+        fetch(`${URL}/details/json?placeid=${place}&key=${API_KEY}`).
+            then(res => res.json()).
+            then(details => {
+                res.status(STATUS_OKAY)
+                console.log('Details', details.result);
+                res.send(details.result);
       })
       .catch(err => console.error(err));
-  })
+  });
+});
+
+
+// server.get('/places', (req,res) => {
+//     fetch(`${URL}/textsearch/json?query=${req.query.term}&key=${API_KEY}`).
+//     then(res => res.json()).
+//     then(json => {
+//         return res.send(json.results);
+//     })
+//     .catch(err => console.error(err));
+// })
 
   server.listen(PORT);
